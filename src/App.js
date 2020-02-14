cf7efe3efec37dadd6b57300ba08e3b72d107cf8
Freddy <freddy.artea@gmail.com>
2020-02-14 16:11:52 -0500

    creando los primeros styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import styled from '@emotion/styled'
 import Header from './components/Header'
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`
 
 
 function App () {
   return(
-    <Header
-      titulo='Cotizador de Seguros' 
-    />
+    <Contenedor>
+      <Header
+        titulo='Cotizador de Seguros' 
+      />
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
+    
   )
 }
 
