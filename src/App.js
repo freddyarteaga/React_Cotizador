08bd7a676bd6f95601d90602607aae4c37782313
Freddy <freddy.artea@gmail.com>
2020-02-14 20:15:57 -0500

    mostrando el total a pagar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
 
 const Contenedor = styled.div`
   max-width: 600px;
@@ -27,7 +28,7 @@ function App () {
   })
 
   // Extraer datos
-  const { datos } = resumen
+  const { cotizacion, datos } = resumen
 
   return(
     <Contenedor>
@@ -40,6 +41,9 @@ function App () {
         />
         <Resumen 
           datos={datos}
+        />
+        <Resultado
+          cotizacion={cotizacion}
         />
       </ContenedorFormulario>
     </Contenedor>
